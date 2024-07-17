0aabd9d3908ee563f6729c3b9d53069eef87d35d
Mark Cherepakha <mvky2004@gmail.com>
2024-07-17 19:58:06 +0300

    del_const

--- constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-MODELS_DIR = os.path.join(BASE_DIR, 'models')
-
-MODEL_FILENAME = 'model.joblib'
-MODEL_PATH = os.path.join(MODELS_DIR, MODEL_FILENAME)
