5ff918cc07f69d1e774b08b1c6e4de48f09a5682
Mark Cherepakha <mvky2004@gmail.com>
2024-08-06 23:09:18 +0300

    nltk_changing

--- utils/nltk_resources.py
+++ utils/nltk_resources.py
@@ -1,8 +1,16 @@
 import nltk
+from nltk.data import find
+from tqdm import tqdm
 
-def download_nltk_resources():
-    nltk.download('punkt', quiet=True)
-    nltk.download('stopwords', quiet=True)
-    nltk.download('wordnet', quiet=True)
 
-download_nltk_resources()
+nltk_packages = ['punkt', 'averaged_perceptron_tagger', 'maxent_ne_chunker', 'words']
+
+def download_nltk_packages(packages):
+    for package in tqdm(packages, desc="Checking and downloading NLTK packages"):
+        try:
+            find(f'tokenizers/{package}')  # Проверяем наличие пакета
+        except LookupError:
+            nltk.download(package)  # Загружаем пакет, если он отсутствует
+
+download_nltk_packages(nltk_packages)
+
